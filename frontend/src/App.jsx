67297e3f2b0146cdcff8e34ba9c4b82447581ed8
Aryan Hansoti <thewebcodrr@gmail.com>
2025-02-09 11:37:19 +0530

    setup - installing libraries (react-router-dom, socket.io, axios)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to MERN Stack Template (Vite)</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
